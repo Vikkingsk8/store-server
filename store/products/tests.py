1bb5d3f140b374ae1bdf569bd117dc5765878849
Vikkingsk8 <viktor.ermilov.96@mail.ru>
2023-02-11 18:24:54 +0300

    Create README.md

--- store/products/tests.py
+++ store/products/tests.py
@@ -1,3 +1,34 @@
+from django.test import TestCase
+from django.urls import reverse
 
 
-# Create your tests here.
+class IndexViewTestCase(TestCase):
+
+    def test_view(self):
+        path = reverse('index')
+        response = self.client.get(path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Store')
+        self.assertTemplateUsed(response, 'products/index.html')
+
+
+class ProductsListViewTestCase(TestCase):
+    fixtures = ['categories.json', 'goods.json']
+
+    def test_list(self):
+        path = reverse('products:index')
+        response = self.client.get(path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'products/products.html')
+        self.assertEqual(response.context_data['title'], 'Store - Каталог')
+        self.assertEqual(list(response.context_data['object_list']), list(products[:3]))
+
+    def test_list_with_category(self):
+        category = ProductCategory.objects.first()
+        path = reverse('products:category', kwargs={'category_id': category.id})
+        response = self.client.get(path)
+
+        products = Product.objects.all()
+
