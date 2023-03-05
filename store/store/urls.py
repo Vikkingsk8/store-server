02f53a56b6f4f04a3c8a643c82fae38ee20db0a1
Vikkingsk8 <viktor.ermilov.96@mail.ru>
2023-03-05 10:44:18 +0300

    Create README.md

--- store/store/urls.py
+++ store/store/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 from products.views import IndexView
+from orders.views import stripe_webhook_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,8 +13,9 @@ urlpatterns = [
     path('users/', include('users.urls', namespace='users')),
     path('accounts/', include('allauth.urls')),
     path('orders/', include('orders.urls', namespace='orders')),
+    path('webhook/stripe', stripe_webhook_view, name='stripe_webhook'),
 ]
 
 if settings.DEBUG == True:
     urlpatterns.append(path('__debug__/', include('debug_toolbar.urls')))
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
